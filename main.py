01a37b95a1bf26c3a5374604da4974e96cf85338
govind-kumawat2002 <kumawatritik54@gmail.com>
2026-01-30 12:48:31 +0530

    dev studio website

---
+++ main.py
@@ -0,0 +1,21 @@
+# from flask import Flask, request, redirect
+
+# app = Flask(__name__)
+
+# @app.route('/contact', methods=['POST'])
+# def contact():
+#     print(request.form)   # data aa raha hai
+#     return redirect("/index.html")
+
+# if __name__ == "__main__":
+#     app.run(port=5000, debug=True)
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    print(request.form)
+    return redirect(request.referrer)  # SAME page
+
+app.run(port=5000, debug=True)
